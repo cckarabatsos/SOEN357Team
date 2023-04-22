6605cb7a28a48ffaf9ab90cff2d789314272be7b
Christos Karabatsos <78325796+cckarabatsos@users.noreply.github.com>
2023-04-21 21:10:21 -0400

    Added comments

--- CMore.py
+++ CMore.py
@@ -4,12 +4,15 @@ from sklearn.cluster import KMeans
 from collections import Counter
 import webcolors
 
+#this function converts from BGR to RGB format 
 def bgr2rgb(color_bgr):
     return tuple(reversed(color_bgr))
 
+#detects the 'k' dominant colors in an image using clustering. Returns a list of RGB tuples representing the dominant colors and dictionary of the color centroids
 def detect_dominant_colors(image, k=5):
     original_shape = image.shape
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # reshape image to 1D array of pixels
     image = image.reshape((image.shape[0] * image.shape[1], 3))
 
     kmeans = KMeans(n_clusters=k, n_init=10)
@@ -24,10 +27,13 @@ def detect_dominant_colors(image, k=5):
 
     return [tuple(map(int, color)) for color in dominant_colors], color_centroids
 
+
+#Converts an RGB color tuple to a color name. Uses the WEBCOLORS library to initiiate the conversion
 def get_color_name(rgb_color):
     try:
         color_name = webcolors.rgb_to_name(rgb_color)
     except ValueError:
+        # if the color is not recognized, find the closest color name
         min_colors = {}
         for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
             r_c, g_c, b_c = webcolors.hex_to_rgb(key)
@@ -39,12 +45,17 @@ def get_color_name(rgb_color):
 
     return color_name
 
+
+#Detects the dominant colors and displays their color names on the frame in a single video frame
 def process_frame(frame):
+    # detect the dominant colors and centroids in the frame
     dominant_colors, color_centroids = detect_dominant_colors(frame, k=5)
 
+    # loop through each dominant color and display its name and centroid on the frame
     for i, color in enumerate(dominant_colors):
         rgb_color = bgr2rgb(color)
         color_name = get_color_name(rgb_color)
+        # set the text position for the color name
         text_position = (10, 40 * (i + 1))
         cv2.putText(frame, f"{color_name}", text_position, cv2.FONT_HERSHEY_SIMPLEX, 1, rgb_color, 2)
 
@@ -55,7 +66,7 @@ def process_frame(frame):
 
     return frame
 
-
+#Main function of the script. Captures video frames, processing them using function proccess_frame above.
 def main():
 
     cap = cv2.VideoCapture(0)
@@ -67,7 +78,8 @@ def main():
     while True:
         ret, frame = cap.read()
         frame_count += 1
-
+        
+        # If the frame was not read successfully, break out of the loop
         if not ret:
             break
 
@@ -79,6 +91,7 @@ def main():
         else:
             cv2.imshow('Color Detection', frame)
 
+         # Exit the loop if the user presses 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
